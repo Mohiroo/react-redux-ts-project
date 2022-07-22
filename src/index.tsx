3f2911b6affc32573fbc72f1ea8d78db5174d730
Mohiroo <mohiro@bk.ru>
2022-07-22 16:34:29 +0300

    setup store

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import {Provider} from 'react-redux';
-import { store } from './store';
+import { setupStore } from './store/store';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
+
+const store = setupStore();
 
 root.render(
   <Provider store={store}>
---
+++ src/store/store.ts
@@ -0,0 +1,19 @@
+import {combineReducers, configureStore} from '@reduxjs/toolkit'
+import { PostAPI } from '../services/PostService'
+import userReducer from './reducers/UserSlice'
+
+const rootReducer = combineReducers({
+  userReducer,
+  [PostAPI.reducerPath]: PostAPI.reducer,
+})
+
+export const setupStore = () => {
+  return configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(PostAPI.middleware)
+  })
+}
+
+export type RootState = ReturnType<typeof rootReducer>
+export type AppStore = ReturnType<typeof setupStore>
+export type AppDispatch = AppStore['dispatch']
