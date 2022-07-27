bb897f2da02ea0384199f6c2e6e75e464ef4f273
Mohiroo <mohiro@bk.ru>
2022-07-27 20:49:00 +0300

    minor changes and styles in PostInput

--- src/components/UI/input/PostInput.tsx
+++ src/components/UI/input/PostInput.tsx
@@ -2,14 +2,14 @@ import React, { FC } from 'react';
 import styles from './PostInput.module.scss';
 
 interface PostInputProps {
-  text: string;
+  value: string;
   placeholder: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 const PostInput: FC<PostInputProps> = (props) => {
   return (
-    <input className={styles.postInput} value={props.text} onChange={props.onChange} type='text' placeholder={props.placeholder}/>
+    <input className={styles.postInput} value={props.value} onChange={props.onChange} type='text' placeholder={props.placeholder}/>
   );
 };
 
