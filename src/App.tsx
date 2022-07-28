2f6b0c36ea9347596df3a88b82d98c14975d3e08
Mohiroo <mohiro@bk.ru>
2022-07-28 22:01:11 +0300

    add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import PostContainer from "./components/PostContainer";
+import { Link, Route, Routes } from "react-router-dom";
+import Post from "./pages/Post/Post";
+import Posts from "./pages/Posts/Posts";
 
 const App = () => {
-
-    return (<div>
-        <PostContainer/>
-    </div>);
+  return (
+    <>
+      <Link to="/posts">Посты</Link>
+      <Link to="/post">Пост</Link>
+      <Routes>
+        <Route path="/posts" element={<Posts />} />
+        <Route path="/post/:id" element={<Post />} />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,20 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
-import {Provider} from 'react-redux';
-import { setupStore } from './store/store';
+import React from "react";
+import "./App.scss";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import { Provider } from "react-redux";
+import { setupStore } from "./store/store";
 
-const container = document.getElementById('root')!;
+const container = document.getElementById("root")!;
 const root = createRoot(container);
 
 const store = setupStore();
 
 root.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>
 );
