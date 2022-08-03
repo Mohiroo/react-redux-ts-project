02515f6206eac0fd9d3a772bf398a61c6af9ebe1
Mohiroo <mohiro@bk.ru>
2022-08-03 22:23:33 +0300

    add request "fetchUserById"

--- src/store/action-creators/FetchData.ts
+++ src/store/action-creators/FetchData.ts
@@ -3,11 +3,25 @@ import axios from "axios";
 import { IUser } from "../../models/IUser";
 
 export const fetchUsers = createAsyncThunk(
-  "user/fetchAll",
+  "users/fetchAll",
   async (_, thunkAPI) => {
     try {
       const response = await axios.get<IUser>(
         "https://jsonplaceholder.typicode.com/users"
+      );
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue("Не удалось загрузить пользователей");
+    }
+  }
+);
+
+export const fetchUserById = createAsyncThunk(
+  "fetchUserById",
+  async (userId: string, thunkAPI) => {
+    try {
+      const response = await axios.get<IUser>(
+        "https://jsonplaceholder.typicode.com/users/" + userId
       );
       return response.data;
     } catch (error) {
