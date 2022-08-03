2db1115cd04ec8cc782d4d117340dadfbfba28db
Mohiroo <mohiro@bk.ru>
2022-08-03 23:11:46 +0300

    add loader to Posts page

--- src/pages/Posts/Posts.tsx
+++ src/pages/Posts/Posts.tsx
@@ -2,10 +2,11 @@ import { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import PostList from "../../components/PostList/PostList";
 import PostInput from "../../components/UI/input/PostInput";
+import Loader from "../../components/UI/loader/Loader";
 import { usePosts } from "../../hooks/useSortedPosts";
 import { IPost } from "../../models/IPost";
 import { PostAPI } from "../../services/PostService";
-import styles from './Posts.module.scss'
+import styles from "./Posts.module.scss";
 
 const Posts = () => {
   const navigate = useNavigate();
@@ -13,7 +14,11 @@ const Posts = () => {
   const [postsLimit, setPostsLimit] = useState<number>(20);
   const [searchQuery, setSearchQuery] = useState<string>("");
 
-  const { data: serverPosts, error, isLoading } = PostAPI.useGetPostsQuery(postsLimit);
+  const {
+    data: serverPosts,
+    error,
+    isLoading,
+  } = PostAPI.useGetPostsQuery(postsLimit);
   const [posts, setPosts] = useState<IPost[]>([]);
   const sortedPosts = usePosts(posts, searchQuery);
 
@@ -35,11 +40,10 @@ const Posts = () => {
     lastElement.current && observer.current.observe(lastElement.current);
   }, [isLoading]);
 
-  const hidePost = (post: IPost) => {
+  const hidePost = (post: IPost) =>
     setPosts(posts.filter((p) => p.id !== post.id));
-  };
 
-  const goToPost = (id: number) => navigate(`/post/${id}`)
+  const goToPost = (id: number) => navigate(`/post/${id}`);
 
   return (
     <div>
@@ -52,8 +56,11 @@ const Posts = () => {
       </div>
       {error && <h1 className={styles.posts__title}>Произошла ошибка</h1>}
       <PostList posts={sortedPosts} hide={hidePost} goToPost={goToPost} />
-      {!error && <div ref={lastElement} className={styles.observer}></div>}
-      {!error && isLoading && <h1  className={styles.posts__title}>Загружаю посты...</h1>}
+      {!error && (
+        <div ref={lastElement} className={styles.observer}>
+          <Loader />
+        </div>
+      )}
     </div>
   );
 };
