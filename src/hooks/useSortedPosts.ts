28f407e74227e141dd13cb1f5f9d3f5ac9811f2d
Mohiroo <mohiro@bk.ru>
2022-08-05 21:02:59 +0300

    edit sorting (no dublicate)

--- src/hooks/useSortedPosts.ts
+++ src/hooks/useSortedPosts.ts
@@ -1,16 +1,15 @@
 import { useMemo } from "react";
 import { IPost } from "../models/IPost";
 
-interface UsePostsState {
-  (posts: IPost[], query: string): IPost[]
-}
-
-export const usePosts: UsePostsState = (posts, query) => {
-
+export const usePosts = (posts: IPost[], query: string): IPost[] => {
   const searchedPosts = useMemo(() => {
-    return [...posts?.filter(post => post.title.toLowerCase().includes(query.toLowerCase())), 
-            ...posts?.filter(post => String(post.id).includes(query))];
-  }, [query, posts])
+    return [
+      ...posts?.filter((post) =>
+        post.title.toLowerCase().includes(query.toLowerCase())
+      ),
+      ...posts?.filter((post) => String(post.id).includes(query)),
+    ].filter((item, pos, arr) => arr.indexOf(item) === pos);
+  }, [query, posts]);
 
   return searchedPosts;
-}
+};
