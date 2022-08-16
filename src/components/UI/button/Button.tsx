62f559080e8e56e264540c5e203dcbf6a688e750
Mohiroo <mohiro@bk.ru>
2022-08-16 22:21:19 +0300

    edit Button

--- src/components/UI/button/Button.tsx
+++ src/components/UI/button/Button.tsx
@@ -1,44 +1,23 @@
 import React, { FC } from "react";
 import styles from "./Button.module.scss";
 
-export const enum ButtonMode {
-  hide,
-  user,
-  default,
+export enum ButtonMode {
+  hide = "btn_hide",
+  user = "btn_user",
+  default = "btn",
 }
 
 interface ButtonProps {
   text: string;
-  mode?: ButtonMode;
+  mode: ButtonMode;
   disabled?: boolean;
   onClick: () => void;
 }
 
 const Button: FC<ButtonProps> = (props) => {
-  let className: string = "";
-
-  switch (props.mode) {
-    case ButtonMode.hide: {
-      className = styles.btn_delete;
-      break;
-    }
-    case ButtonMode.user: {
-      className = styles.btn_user;
-      break;
-    }
-    case ButtonMode.default: {
-      className = styles.btn;
-      break;
-    }
-    default: {
-      className = styles.btn;
-      break;
-    }
-  }
-
-  className = props.disabled
-    ? `${className} ${styles.btn_disabled}`
-    : className;
+  let className: string = props.disabled
+    ? `${styles[props.mode]} ${styles.btn_disabled}`
+    : styles[props.mode];
 
   return (
     <button className={className} onClick={props.onClick}>
