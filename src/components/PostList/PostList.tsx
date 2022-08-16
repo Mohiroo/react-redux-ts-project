0e8caa9a671820f19def497d4ce9cbd55c128ef8
Mohiroo <mohiro@bk.ru>
2022-08-16 19:23:52 +0300

    restructuring PostList for animation

--- src/components/PostList/PostList.tsx
+++ src/components/PostList/PostList.tsx
@@ -1,4 +1,3 @@
-import React, { useState } from "react";
 import { IPost } from "../../models/IPost";
 import PostItem from "../PostItem/PostItem";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
@@ -11,24 +10,28 @@ interface PostContainerProps {
   goToPost: (id: number) => void;
 }
 
-export const PostList: React.FC<PostContainerProps> = ({posts, hide, goToPost}) => {
-  if (!posts?.length)
-    return (
-      <h1 className={styles.post_list__title}>
-        Посты отсутствуют
-      </h1>
-    );
-
+export const PostList: React.FC<PostContainerProps> = (props) => {
   return (
     <div className={styles.post_list}>
-      <h1 className={styles.post_list__title}>Посты</h1>
-      <TransitionGroup>
-        {posts?.map((post) => (
-          <CSSTransition key={post.id} timeout={500} classNames="post">
-            <PostItem key={post.id} post={post} hide={hide} goToPost={goToPost} />
-          </CSSTransition>
-        ))}
-      </TransitionGroup>
+      {props.posts?.length ? (
+        <>
+          <h1 className={styles.post_list__title}>Посты</h1>
+          <TransitionGroup>
+            {props.posts?.map((post) => (
+              <CSSTransition key={post.id} timeout={500} classNames="post">
+                <PostItem
+                  key={post.id}
+                  post={post}
+                  hide={props.hide}
+                  goToPost={props.goToPost}
+                />
+              </CSSTransition>
+            ))}
+          </TransitionGroup>
+        </>
+      ) : (
+        <h1 className={styles.post_list__title}>Посты отсутствуют</h1>
+      )}
     </div>
   );
 };
