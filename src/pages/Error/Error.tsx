bc93a5ea7b8a27e06cc23d5236602b2d7e9d4387
Mohiroo <mohiro@bk.ru>
2022-08-17 18:54:04 +0300

    add new error mode

--- src/pages/Error/Error.tsx
+++ src/pages/Error/Error.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
-import styles from './Error.module.scss'
+import { useAppSelector } from "../../hooks/useTypedSelector";
+import { RootState } from "../../store/store";
+import styles from "./Error.module.scss";
 
 const Error = () => {
+  const isAuth = useAppSelector((state: RootState) => state.AuthReducer.isAuth);
+
   return (
     <h1 className={styles.error_page}>
-      Данной страницы не существует
+      {isAuth ? `Данной страницы не существует` : `Войдите в аккаунт`}
     </h1>
   );
 };
