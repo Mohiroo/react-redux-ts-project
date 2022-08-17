8153d454e60d5a290dc8472e142a420eb589e9fd
Mohiroo <mohiro@bk.ru>
2022-08-17 18:55:29 +0300

    add Login page

---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,48 @@
+import { FC } from "react";
+import { useDispatch } from "react-redux";
+import { useLocation, useNavigate } from "react-router-dom";
+import { login } from "../../store/reducers/AuthSlice";
+import styles from "./Login.module.scss";
+
+interface stateType {
+  pathname: string | null;
+}
+
+const Login: FC = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const pathname = location.state !== null ? (location.state as stateType).pathname! : '/';
+
+  return (
+    <div className={styles.login}>
+      <h1 className={styles.login__title}>Авторизация</h1>
+
+      <input
+        className={styles.login__input}
+        type={"text"}
+        placeholder={"Логин"}
+      />
+      <input
+        className={styles.login__input}
+        type={"password"}
+        autoComplete={"on"}
+        placeholder={"Пароль"}
+      />
+      <button
+        className={styles.login__button}
+        onClick={() => {
+          dispatch(login());
+          setTimeout(() => {
+            navigate(pathname);
+          }, 200);
+        }}
+      >
+        Войти
+      </button>
+    </div>
+  );
+};
+
+export default Login;
